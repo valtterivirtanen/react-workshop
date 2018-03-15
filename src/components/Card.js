d9ea3105960525864ba8a6eccb9fb04c8ccf5f47
Valtteri Virtanen <valtteri.virtane@gmail.com>
2018-03-15 16:43:30 +0200

    Added hide button possibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+
+
 const CardWrapper = styled.div`
     border: 1px solid black;
-    padding: 10px
+    padding: 10px;
+    visibility: ${props => props.isVisible ? 'visible':'hidden'};
+`;
+
+const CardHeader = styled.h1`
+    margin: 10% 0;
+    text-decoration: underline;
 `;
 
 const Image = styled.img`
     max-width: 100%;
-    clip: rect(100px,100px,200px,200px);
 `;
 
 export default class Card extends React.Component {
     render() {
         return (
-            <CardWrapper>
-                <h1>{this.props.title}</h1>
+            <CardWrapper isVisible={this.props.cardVisibility}>
+                <CardHeader>{this.props.title}</CardHeader>
                 <Image src={this.props.imageUrl}/>
             </CardWrapper>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 import Card from '../components/Card.js'
+import Button from "../components/Button.js";
 
-export default ({ data }) => <div>
-    <Card title='My title' imageUrl='http://az831865.vo.msecnd.net/images/video/bc_intro_video_still.jpg?v=w2CvG_IIZtV4ebJnwRqUTiBgXM0ccTF4JJHDeWSUs1c'/>
-</div>;
+export default class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cardVisible: true
+        }
+    }
+
+    handleClick = (event) => {
+        console.log(this.state);
+        this.setState((prevState) => ({
+            cardVisible: !prevState.cardVisible
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+               <Card cardVisibility={this.state.cardVisible} title='My card title' imageUrl='http://az831865.vo.msecnd.net/images/video/bc_intro_video_still.jpg?v=w2CvG_IIZtV4ebJnwRqUTiBgXM0ccTF4JJHDeWSUs1c'/>
+               <Button onClick={this.handleClick}>Click to hide card </Button>
+            </div>
+        )
+    }
+}
